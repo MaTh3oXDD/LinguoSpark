1e593f0b95775bc4be521b03245f2239625210d2
unknown <Mateusz Jedrzejczak>
2024-06-13 18:18:57 +0200

    Stworzenie klasy User

---
+++ src/main/java/org/example/Class/Main.java
@@ -0,0 +1,9 @@
+package org.example.Class;
+
+import org.example.Swing.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/Swing/Menu.java
@@ -0,0 +1,10 @@
+package org.example.Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame {
+
+}
